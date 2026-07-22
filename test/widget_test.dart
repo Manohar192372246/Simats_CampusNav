5d4e3185b855f4a7cf02d1aa04161292738e9243
Manohar <manohar@example.com>
2026-07-22 09:32:23 +0530

    Fix package import name in tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:simats_campusnav/main.dart';
+import 'package:campus_navigation/main.dart';
 
 void main() {
   group('Comprehensive UI & Logic Tests', () {
